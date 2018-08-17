116cc850d7b87b684ed3464687cb5114a8270180
tmaculmendes <mara.macul.mendes@oracle.com>
2018-08-17 10:52:04 -0400

    removal of language to system

--- pyresttest/resttest.py
+++ pyresttest/resttest.py
@@ -115,8 +115,6 @@ class TestConfig:
     # NEW
     oci_signature = True
     oci_key = None
-    os.environ["LANG"] = "en_US.UTF-8"
-    os.environ["LANGUAGE"] = "en_US:en"
 
     # Binding and creation of generators
     variable_binds = None
